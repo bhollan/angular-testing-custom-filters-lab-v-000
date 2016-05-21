0438e761d143f35b3cdaa272a1f098a211b5cce5
Brian Holland <beehollander@gmail.com>
2016-05-21 08:57:19 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,27 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+  var $controller;
 
-	beforeEach(module('app'));
+  beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($injector) {
+    $filter = $injector.get('$filter');
+    favoriteFood = $filter('favoriteFood');
 
-	
+    fakePeople = [
+    {name: 'Steve',
+      favoriteFood: 'apples'},
+    {name: 'John',
+      favoriteFood: 'bagels'},
+    {name: 'Tim',
+      favoriteFood: 'french fries'}
+    ];
+
+  }));
+
+  it('should filter for favoriteFood = "bagels"', function(){
+    var results = favoriteFood(fakePeople, 'bagels');
+
+    expect(results).toEqual([{name:'John', favoriteFood:'bagels'}]);
+  });
+
 });
